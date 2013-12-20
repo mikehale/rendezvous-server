b5cf1db3152ba0d7d5f627b9a56864cd501fb26e
Michael Hale <mike@hales.ws>
2013-12-20 09:22:32 -0500

    x

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+set :peers, []
+
+helpers do
+  def peer_pair(socket)
+    [socket.peeraddr[3], socket.peeraddr[1]].join(":")
+  end
+end
+
+get '/' do
+  socket = request.env["puma.socket"]
+  puts peer_pair(socket)
+  peer_pair(socket)
+end
