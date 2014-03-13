179cd2be8b365208fc94aadb7583fbeeccdea8d4
Michael Hale <mike@hales.ws>
2014-03-13 04:49:26 -0400

    prefer forwarded for header

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ class RendezvousServer < Sinatra::Base
   @@redis = nil
 
   helpers do
-    def peer_pair(socket)
-      [socket.peeraddr[3], socket.peeraddr[1]].join(":")
+    def peer_pair(socket, forwarded_for)
+      [forwarded_for || socket.peeraddr[3], socket.peeraddr[1]].join(":")
     end
 
     def redis
@@ -21,7 +21,7 @@ class RendezvousServer < Sinatra::Base
   end
 
   get '/' do
-    addr = peer_pair(request.env["puma.socket"])
+    addr = peer_pair(request.env["puma.socket"], request.env["HTTP_X_FORWARDED_FOR"])
 
     if (peers = redis.smembers("peers")).size > 0
       redis.srem("peers", peers)
