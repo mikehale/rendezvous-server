27dea11e5cc2fc046def09d6043ee6d3069066d6
Michael Hale <mike@hales.ws>
2014-03-24 21:19:41 -0400

    wip: pg not redis

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require 'sinatra/base'
-require 'redis'
+require 'pg'
 require 'uri'
 
 class RendezvousServer < Sinatra::Base
-  @@redis = nil
+  @@pg = nil
 
   helpers do
     def peer_pair(env)
@@ -14,32 +14,30 @@ class RendezvousServer < Sinatra::Base
       [forwarded_for || socket.peeraddr[3], forwarded_port || socket.peeraddr[1]].join(":")
     end
 
-    def redis
-      unless @@redis
-        uri = URI.parse(ENV['REDIS_URL'] || "localhost:6379")
-        @@redis = Redis.new(:host => uri.host, :port => uri.port)
-      end
-
-      @@redis
+    def pg
+      @@pg ||= PG.connect(ENV['DATABASE_URL'] | "postgres://localhost/rendezvous-server")
     end
   end
 
   get '/' do
     addr = peer_pair(request.env)
+    puts addr.inspect
 
-    if (peers = redis.smembers("peers")).size > 0
-      redis.srem("peers", peers)
-      redis.sadd("peers", addr)
-      puts "peer1: #{peers.first}"
-      peers.first
-    else
-      redis.sadd("peers", addr)
-      while (peers = redis.smembers("peers").reject{|e| e == addr }).empty?
-        sleep 0.1
-      end
-      redis.srem("peers", peers)
-      puts "peer2: #{peers.first}"
-      peers.first
-    end
+    # if (peers = redis.smembers("peers")).size > 0
+    #   redis.srem("peers", peers)
+    #   redis.sadd("peers", addr)
+    #   puts "peer1: #{peers.first}"
+    #   peers.first
+    # else
+    #   redis.sadd("peers", addr)
+    #   while (peers = redis.smembers("peers").reject{|e| e == addr }).empty?
+    #     sleep 0.1
+    #   end
+    #   redis.srem("peers", peers)
+    #   puts "peer2: #{peers.first}"
+    #   peers.first
+    # end
+
+    ''
   end
 end
